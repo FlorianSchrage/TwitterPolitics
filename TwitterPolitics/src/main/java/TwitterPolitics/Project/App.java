a07cc1a6bbe701403af76c807692b2cc6fcbe59b
FlorianSchrage <florian.schrage@sap.com>
2017-12-13 10:05:10 +0100

    Test

---
+++ TwitterPolitics/src/main/java/TwitterPolitics/Project/App.java
@@ -0,0 +1,13 @@
+package TwitterPolitics.Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!123" );
+    }
+}
