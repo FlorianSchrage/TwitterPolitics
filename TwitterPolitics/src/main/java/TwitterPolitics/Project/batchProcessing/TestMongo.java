abf4b1a1f04e0259759ab99c385dead9ab96c7e6
Steffen Terheiden <Steffen Terheiden@SOBIS-089-NBK.sobis.com>
2017-12-15 20:57:52 +0100

    add mongo client

---
+++ TwitterPolitics/src/main/java/TwitterPolitics/Project/batchProcessing/TestMongo.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package TwitterPolitics.Project.batchProcessing;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.bson.Document;
+
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+
+public class TestMongo {
+
+	public static final String DB_NAME = "twitterTopics";
+
+	public enum Collections {
+		TWEETS("tweets"), RESULTS("results");
+
+		Collections(String name) {
+			collectionName = name;
+		}
+
+		public String getCollectionName() {
+			return collectionName;
+		}
+
+		private String collectionName;
+	}
+
+	private static MongoClient getMongoDBClient() {
+		MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
+		return new MongoClient(connectionString);
+	}
+
+	private static MongoCollection<Document> getCollection(Collections collection) {
+		return getMongoDBClient().getDatabase(DB_NAME).getCollection(collection.getCollectionName());
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void saveToMongo(JavaPairDStream<String, String> stream, Collections collection) {
+		stream.toJavaDStream().map(f -> new Document(f._1, f._2)).foreachRDD(new Function<JavaRDD<Document>, Void>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Void call(JavaRDD<Document> data) throws Exception {
+				System.out.println("Before insert: " + getCollection(collection).count() + " documents found. " + data.count() + " data elements");
+				if (data != null && data.count() > 0) {
+					getCollection(collection).insertMany(data.collect());
+					System.out.println("Inserted Data Done");
+				}
+				System.out.println("After insert: " + getCollection(collection).count() + " documents found");
+				return null;
+			}
+
+		});
+
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void readAllData(Collections collection) {
+		System.out.println("Print all data in " + collection.getCollectionName());
+		getCollection(collection).find().forEach((Block<Document>) d -> {
+			System.out.println(d.toJson());
+		});
+	}
+}
--- TwitterPolitics/src/main/java/TwitterPolitics/Project/streamProcessing/StreamProcessor.java
+++ TwitterPolitics/src/main/java/TwitterPolitics/Project/streamProcessing/StreamProcessor.java
@@ -1,75 +1,74 @@
 package TwitterPolitics.Project.streamProcessing;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
-import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 
-import TwitterPolitics.Project.streamIngestion.KafkaTwitterIngestion;
-import TwitterPolitics.Project.streamIngestion.Record;
+import TwitterPolitics.Project.batchProcessing.TestMongo;
 import kafka.serializer.StringDecoder;
 import scala.Tuple2;
 
 public class StreamProcessor {
-	
+
 	private static final String HADOOP_COMMON_PATH = "C:/Users/D060237/git/BDM_Lab3_Training/src/main/resources/winutils";
 	public static final String TOPIC = "Politics";
-	
-	public void secondDraft()
-	{
+
+	public void secondDraft() {
 		System.setProperty("hadoop.home.dir", HADOOP_COMMON_PATH);
-        SparkConf sparkConfig = new SparkConf().setAppName("TwitterPolitics").setMaster("local[*]");
-        JavaSparkContext sparkCtx = new JavaSparkContext(sparkConfig);
-        JavaStreamingContext jStreamCtx = new JavaStreamingContext(sparkCtx, new Duration(1000));
+		SparkConf sparkConfig = new SparkConf()
+				.setAppName("TwitterPolitics")
+				.setMaster("local[*]");
+		JavaSparkContext sparkCtx = new JavaSparkContext(sparkConfig);
+		JavaStreamingContext jStreamCtx = new JavaStreamingContext(sparkCtx, new Duration(1000));
 
-//        HashMap<String, Integer> topicsAndReplicas = new HashMap<String, Integer>();
-//        topicsAndReplicas.put("Sample", 10);
-        
-        //localhost:2181
-        //GroupId muss nur unique sein
-//        JavaPairReceiverInputDStream<String, String> record = KafkaUtils.createStream(jStreamCtx, "localhost:2181", "Group1", topicsAndReplicas);
-        
+		// HashMap<String, Integer> topicsAndReplicas = new HashMap<String, Integer>();
+		// topicsAndReplicas.put("Sample", 10);
 
-        Map<String, String> kafkaParams = new HashMap<>();
-        kafkaParams.put("metadata.broker.list", "localhost:9092");
-        Set<String> topics = Collections.singleton(TOPIC);
+		// localhost:2181
+		// GroupId muss nur unique sein
+		// JavaPairReceiverInputDStream<String, String> record = KafkaUtils.createStream(jStreamCtx, "localhost:2181", "Group1",
+		// topicsAndReplicas);
 
-        JavaPairInputDStream<String, String> record = KafkaUtils.createDirectStream(jStreamCtx,
-        		String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaParams, topics);
+		Map<String, String> kafkaParams = new HashMap<>();
+		kafkaParams.put("metadata.broker.list", "localhost:9092");
+		Set<String> topics = Collections.singleton(TOPIC);
 
-        
-        System.out.println("I will print...");
-        record.print();
-        
-        record.mapToPair(f -> {
-//        	Record thisRecord = Record.getByJsonString(f._2);
-        	System.out.println("Processing " + f._2);
-//        	System.out.println("Text: " + thisRecord.getText());
-//        	System.out.println("Language: " + thisRecord.getLanguage());
-//        	System.out.println("Location: " + thisRecord.getLocation().toString());
-//        	System.out.println("Place: " + thisRecord.getPlace().toString());
-        	return new Tuple2<String, String>(f._1 + "/////" + f._2 ,"Hello World!");
-        }).print();
-        
-        jStreamCtx.start();
-        jStreamCtx.awaitTermination();
+		JavaPairInputDStream<String, String> record = KafkaUtils.createDirectStream(jStreamCtx,
+				String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaParams, topics);
+
+		System.out.println("I will print...");
+		record.print();
+
+		JavaPairDStream<String, String> stream = record.mapToPair(f -> {
+			// Record thisRecord = Record.getByJsonString(f._2);
+			System.out.println("Processing " + f._2);
+			// System.out.println("Text: " + thisRecord.getText());
+			// System.out.println("Language: " + thisRecord.getLanguage());
+			// System.out.println("Location: " + thisRecord.getLocation().toString());
+			// System.out.println("Place: " + thisRecord.getPlace().toString());
+			return new Tuple2<>(f._1, f._2);
+		});
+
+		TestMongo.saveToMongo(stream, TestMongo.Collections.TWEETS);
+		System.out.println("TEststdasgsdg");
+		TestMongo.readAllData(TestMongo.Collections.TWEETS);
+
+		jStreamCtx.start();
+		jStreamCtx.awaitTermination();
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Consumer running...");
-		
+
 		StreamProcessor processor = new StreamProcessor();
 		processor.secondDraft();
 	}
